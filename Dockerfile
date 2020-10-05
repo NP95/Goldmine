ce20f84e4fcd76f16ef9044321cb41dc9698c136
Debjit Pal <work.debjitpal@gmail.com>
2020-10-04 20:43:30 -0400

    Enabling formal verification toolchain for GoldMine. Docker supported. No guideline for Conda/Standalone. The support may be removed.

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,23 @@ MAINTAINER Debjit Pal <work.debjitpal@gmail.com>
 # Prepping the Fedora Docker container with upgraded packages and base system packages
 RUN dnf clean all
 RUN dnf -y update
-RUN dnf -y install python2 python2-devel python2-pygraphviz gcc gcc-c++ vim-enhanced iverilog wget
+RUN dnf -y install python2 python2-devel python2-pygraphviz gcc gcc-c++ vim-enhanced iverilog wget git make clang flex bison autoconf gperf cmake boost-devel boost-static
+RUN dnf -y install glibc-static
+RUN dnf -y install readline-devel tcl-devel gmp-devel gmp-static libffi-devel
+RUN dnf -y install libcxx-static libcxxabi-static compat-libstdc++-33 compat-libstdc++-33.i686 libstdc++-static
 
 # Copying GoldMine code to the Docker container
 ADD ./ /opt/goldmine
 WORKDIR /opt/goldmine
+
+# Installing Formal Verification toolchain
+RUN sh build_tools.sh
+ENV PATH /opt/goldmine/tool/abc:$PATH
+ENV PATH /opt/goldmine/tool/boolector/build/bin:$PATH
+ENV PATH /opt/goldmine/tool/extavy/build/avy/src:$PATH
+ENV PATH /opt/goldmine/tool/z3/build:$PATH
+ENV PATH /opt/goldmine/tool/yosys:$PATH
+ENV PATH /opt/goldmine/tool/yices2/build/x86_64-pc-linux-gnu-release/bin:$PATH
 
 # Installing GoldMine specific Python packages to the Container
 RUN pip install -r requirements.txt
---
+++ build_tools.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+PROC=64
+
+## Make tool installation directory
+mkdir -pv tool
+cd tool
+
+## ABC installation
+git clone https://github.com/berkeley-abc/abc.git
+cd abc
+make
+cd ..
+
+## YoSys instlation
+git clone https://github.com/YosysHQ/yosys.git yosys
+cd yosys
+make -j${PROC}
+cd ..
+
+## SymbiYosys installation
+git clone https://github.com/YosysHQ/SymbiYosys.git SymbiYosys
+cd SymbiYosys
+make install
+cd ..
+
+## Yices 2 installation
+git clone https://github.com/SRI-CSL/yices2.git yices2
+cd yices2
+autoconf
+./configure
+make -j64
+cd ..
+
+## Z3 instalation
+git clone https://github.com/Z3Prover/z3.git z3
+cd z3
+python scripts/mk_make.py
+cd build
+make -j${PROC}
+cd ../../
+
+## super_prove installation
+wget https://downloads.bvsrc.org/super_prove/super_prove-hwmcc17_final-2-d7b71160dddb-CentOS_7-Release.tar.gz
+tar -xzvf super_prove-hwmcc17_final-2-d7b71160dddb-CentOS_7-Release.tar.gz
+rm -rf super_prove-hwmcc17_final-2-d7b71160dddb-CentOS_7-Release.tar.gz
+cd super_prove/bin
+FILE="suprove"
+printf '%s\n' '#!/bin/bash' \
+'tool=super_prove; if [ "$1" != "${1#+}" ]; then tool="${1#+}"; shift; fi' \
+'exec /opt/goldmine/tool/super_prove/bin/${tool}.sh "$@"' > ${FILE}
+/usr/bin/chmod +x ${FILE}
+cd ../..
+
+## Avy instllation
+git clone https://bitbucket.org/arieg/extavy.git
+cd extavy
+git submodule update --init
+mkdir -pv build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j64
+cd ../..
+
+## Boolector installation
+git clone https://github.com/boolector/boolector
+cd boolector
+./contrib/setup-btor2tools.sh
+./contrib/setup-lingeling.sh
+./configure.sh
+make -C build -j${PROC}
+cd ..
