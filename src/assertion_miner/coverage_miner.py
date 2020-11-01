818dfb7b2330d35e2d9cb2e4abdb2dde3197d333
Debjit Pal <work.debjitpal@gmail.com>
2020-11-01 18:39:12 -0500

    Coverage miner code day 2

--- src/assertion_miner/coverage_miner.py
+++ src/assertion_miner/coverage_miner.py
@@ -1,14 +1,13 @@
 import pprint as pp
+import pandas as pd
 
 def coverage_miner(features, target, target_feature_df, gmin, gthreshold, cthreshold):
 
-    print("Inside coverage miner")
-    #print(features)
-    #print(target)
-    #print(target_feature_df.iloc[0])
-    print(target_feature_df[target_feature_df['[1]state'] == 1])
-    print(target_feature_df[target_feature_df['[1]state'] == 1].index.tolist())
-    exit(0)
+    features = ['a', 'b', 'c']
+    target = 'z'
+    
+    data = [[0,0,0,0], [0,1,1,0], [1,0,1,0], [1,1,0,1]]
+    target_feature_df = pd.DataFrame(data, columns = ['a', 'b', 'c', 'z'])
 
     covAs = 0.0
     TRows = target_feature_df.shape[0]
@@ -25,13 +24,13 @@ def coverage_miner(features, target, target_feature_df, gmin, gthreshold, cthres
 
     '''
     As = []
-    P = []
-    current_covered_rows = []
-    previous_covered_rows = []
+    curr_covered_rows = []
+    prev_covered_rows = []
     '''
     [('[1]state', 0), ('[1]state', 1), ('[2]req2', 0), ('[2]req2', 1), ('[2]req1', 0),..]
     '''
     F = create_var_val_pair(features)
+    
     while True:
         if gmin < gthreshold or covAs > cthreshold:
             break
@@ -40,13 +39,16 @@ def coverage_miner(features, target, target_feature_df, gmin, gthreshold, cthres
             # F: set of (feature var, val) pair not in P
             # E: simulation trace. Here target_feature_df
             Ac = []
-            gen_candidates(F, P, target, target_feature_df, Ac, gmin)
+            Ac = gen_candidates(F, [], target, target_feature_df, Ac, gmin)
             print(Ac)
-            exit(0)
-            As, current_covered_rows = recalibrate_add(Ac, As, previous_covered_rows, gmin)
+            As, curr_covered_rows = recalibrate_add(Ac, \
+                    As, target_feature_df, prev_covered_rows, TRows, gmin)
             gmin = gmin / 2.0
+            covAs = 1.0 * len(curr_covered_rows) / TRows
+            print('New coverage: ', covAs)
+            print('New gmin: ', gmin)
             del Ac
-
+            prev_covered_rows = curr_covered_rows
 
     return As
 
@@ -62,50 +64,58 @@ def create_var_val_pair(features):
 
 def gen_candidates(F, P, target, E, Ac, gmin):
 
-    #for fi in F:
-    print(F)
-    print(P)
-    while F:
-        fi = F.pop()
-        if 1.0 * 1 / len(P + [fi]) >= gmin:
-            P = P + [fi]
-            if check_consistency(E, P, target, 0):
-                print('True consistency check') 
-                Ac = Ac + [P, list(tuple(target, 0))]
-            elif check_consistency(E, P, target, 1):
-                print('False consistency check') 
-                Ac = Ac + [P, list(tuple(target, 1))]
+    for fi in F:
+        #fi = F.pop()
+        if 1.0 * 1 / pow(2, len(list(set(P + [fi])))) >= gmin:
+            P_ = list(set(P + [fi]))
+            print('Current gain in gen_candidates: ', 1.0 * 1 / pow(2, len(P_)))
+            check_consistency_, tval = check_consistency(E, P_, target)
+            if check_consistency_:
+                Ac.append([P_, tuple([target, tval])])
+                print(Ac)
             else:
-                print('Recursing') 
-                gen_candidates(list(set(F) - set(fi)), P, target, E, Ac, gmin)
+                newF = list(set(F) - set([fi]))
+                gen_candidates(newF, P_, target, E, Ac, gmin)
 
-    return
+    return Ac
 
-def recalibrate_add(Ac, As, previous_covered_rows, gmin):
+def recalibrate_add(Ac, As, E, prev_covered_rows, TRows, gmin):
     
     for a in Ac:
-        if gain(As, a) >= gmin:
+        gain, addl_covered_rows = calc_gain(E, prev_covered_rows, TRows, a)
+        if gain >= gmin:
+            prev_covered_rows = list(set(prev_covered_rows + addl_covered_rows))
             As = As + a
-    return
+    return As, prev_covered_rows
 
-def gain(As, a):
-
-
-    return False
-
-
-def check_consistency(E, P, target, tval):
+def calc_gain(E, prev_covered_rows, TRows, a):
+    
+    new_covered_rows = []
     E_local = E.copy(deep=True)
+    P = a[0]
+    for p in P:
+        attribute = p[0]
+        pval = p[1]
+        E_local = E_local[E_local[attribute] == pval]
+
+    new_covered_rows = E_local.index.tolist()
+
+    addl_covered_rows = list(set(new_covered_rows) - set(prev_covered_rows))
+
+    return 1.0 * len(addl_covered_rows) / TRows, addl_covered_rows
+
+def check_consistency(E, P, target):
+    E_local = E.copy(deep=True)
+    print(P)
     for p in P:
         attribute = p[0]
         pval = p[1]
         E_local = E_local[E_local[attribute] == pval] 
     
-    uniq_val = E_local[target].drop_duplicates().tolist()
-    if len(uniq_val) > 1:
-        return False
+    uniq_val = E_local[target].drop_duplicates().values.tolist()
+    if len(uniq_val) > 1 or len(uniq_val) < 1:
+        #print('More the one unique val')
+        return False, -1
     else:
-        if tval in uniq_val:
-            return True
-        else:
-            return False
+        #print('Uniq val: ', uniq_val[0])
+        return True, uniq_val[0]
